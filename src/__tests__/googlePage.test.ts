f6175f2585a682a57e756f4d92f889ce2d357b23
ItsDresAccount <andrew.smith@devmounta.in>
2020-12-14 20:05:57 -0700

    fs Examples added

---
+++ src/__tests__/googlePage.test.ts
@@ -0,0 +1,27 @@
+import { Driver } from 'selenium-webdriver/chrome'
+import { Google } from '../pageObjects/googlePage'
+const fs = require('fs')
+
+const google = new Google()
+
+test("Does a search", async () => {
+    await google.navigate()
+    await google.search('Cyberpunk Reviews')
+    let myText = await google.getResults()
+    expect(myText.toLowerCase()).toContain('cyberpunk 2077 review')
+    fs.writeFile(`${__dirname}/../dataOutput/google.png`,
+        await google.driver.takeScreenshot(),
+        "base64",
+        (e) => {
+            if (e) console.error(e)
+            else console.log('Image Saved')
+        }
+    )
+    fs.writeFile(`${__dirname}/../dataOutput/test.txt`, myText, (e) => {
+        if (e) console.error(e)
+        else console.log('File Saved')
+    })
+})
+afterAll(async () => {
+    await google.driver.quit()
+})
